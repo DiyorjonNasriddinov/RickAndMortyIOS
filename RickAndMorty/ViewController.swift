6f55ad0041793a9d957f74c7b167c902694cac58
Diyorjon Nasriddinov <diyorjonnasriddinov@gmai.com>
2023-07-02 21:09:56 +0500

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Diyorjon Nasriddinov on 02/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
